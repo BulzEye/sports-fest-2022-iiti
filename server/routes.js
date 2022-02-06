6dc1e70ed965773833ffbcc843f402f24ec10867
Jaisurya <jaisuryak11@gmail.com>
2022-02-07 03:12:22 +0530

    models and routing done

--- server/routes.js
+++ server/routes.js
@@ -1,33 +1,67 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = express.Router();
+const router = require('express').Router();
+// const mongoose = require('mongoose');
+const Events = require('./models').eventModel;
+const Partners = require('./models').partnerModel;
+const Sponsors = require('./models').sponsorModel;
 
 //route handlers
-router.get("/", (req, res, next) => {
-    res.end();
+//Request on Home Page
+router.get("/home", async (req, res, next) => {
+    let response = {};
+    await Events.find({})
+        .sort({ _id: 'desc' })
+        .select('title images')
+        .then(events => {
+            response['events'] = events;
+        })
+        .catch(next);
+
+    await Partners.find({})
+        .sort({ _id: 'desc' })
+        .select('title image type')
+        .then(partners => {
+            response['partners'] = partners;
+        })
+        .catch(next);
+
+    await Sponsors.find({})
+        .sort({ _id: 'desc' })
+        .select('title image type')
+        .then(sponsors => {
+            response['sponsors'] = sponsors;
+        })
+        .catch(next);
+
+    res.status(200).send(response);
 })
 
-router.get('/events', (req, res, next) => {
-    res.status(200).end();
+//Individual Event Request
+router.get('/event/:id', (req, res, next) => {
+    Events.findById(req.params.id)
+        .then(event => {
+            res.status(200).send(event);
+        })
+        .catch(next);
 });
 
-
-
-
-router.get("/events", (req, res, next) => {
-    res.end();
-})
-
-
-
+//Sponsor List Request
 router.get("/sponsors", (req, res, next) => {
-    res.end();
+    Sponsors.find({})
+        .sort({ _id: 'desc' })
+        .then(sponsors => {
+            res.status(200).send(sponsors);
+        })
+        .catch(next);
 })
 
-
-
+//Partners List Request
 router.get("/partners", (req, res, next) => {
-    res.end();
+    Partners.find({})
+        .sort({ _id: 'desc' })
+        .then(partners => {
+            res.status(200).send(partners);
+        })
+        .catch(next);
 })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,21 +4,31 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
+//Connecting to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .catch(err => {
         console.log(err.message)
     });
 
+//Parser and routing
 app.use(express.json());
 app.use('/api', require('./routes'));
 
-// if (process.env.NODE_ENV === 'production') {
-app.use('/', express.static(__dirname + '../frontend/'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'..','frontend','hi.txt'));
-})
-// }
+//Serving static files
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(__dirname + '../frontend/'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+    })
+}
 
+//Error Handling
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).send("Something went wrong...");
+})
+
+//Listening to requests
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
